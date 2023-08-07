f9bf297330faa26ccd23fbd172fc867f56e0a2ec
MilanoBeer <tgd5753@gmail.com>
2023-08-07 18:35:19 +0900

    init: project setting

---
+++ src/test/java/com/shy/sbank/SbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shy.sbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
