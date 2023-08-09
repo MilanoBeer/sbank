e41bb1710fc844d700e62587e27997e1f65d0367
MilanoBeer <tgd5753@gmail.com>
2023-08-09 12:28:24 +0900

    feat: make Account, Product Entity, MemberLoginDto

---
+++ src/main/java/com/shy/sbank/member/exception/PasswordMatchException.java
@@ -0,0 +1,14 @@
+package com.shy.sbank.member.exception;
+
+// 언체크 예외 상속받기
+public class PasswordMatchException extends RuntimeException{
+    // 예외 기본 생성자
+    public PasswordMatchException() {
+        super();
+    }
+
+    // 메세지 출력 용도
+    public PasswordMatchException(String message) {
+        super(message);
+    }
+}
