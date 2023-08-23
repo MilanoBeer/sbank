d18716913e7e89777ac271fb7cc5586fc5c76808
MilanoBeer <tgd5753@gmail.com>
2023-08-24 03:10:35 +0900

    feat: email check, exception

--- src/main/java/com/shy/sbank/member/service/MemberService.java
+++ src/main/java/com/shy/sbank/member/service/MemberService.java
@@ -5,11 +5,14 @@ import com.shy.sbank.member.dto.MemberRegisterDto;
 import com.shy.sbank.member.entity.Member;
 import com.shy.sbank.member.exception.PasswordMatchException;
 import com.shy.sbank.member.repository.MemberRepository;
+import com.shy.sbank.member.repository.MemberRepository_old;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 @Transactional
@@ -21,7 +24,10 @@ public class MemberService {
 
     public boolean register(MemberRegisterDto dto) {
         // TODO : 이메일 중복 처리 ( "이미 가입된 이메일이 존재합니다" )
+        if (memberRepository.existsByEmail(dto.getEmail())) throw new IllegalStateException("이미 가입된 정보가 존재합니다.");
+
         Member member = Member.builder()
+                .name(dto.getName())
                 .email(dto.getEmail())
                 .password(BCrypt.hashpw(dto.getPassword(), BCrypt.gensalt()))// password, salt
                 .build();
@@ -33,7 +39,8 @@ public class MemberService {
 
     public Member login(MemberLoginDto dto) {
         // 1. login정보 확인하기 위해, dto의 email -> repo -> db에서 찾아오기
-        Member loginMember = memberRepository.findByEmail(dto.getEmail());
+        Member loginMember = memberRepository.findByEmail(dto.getEmail())
+                .orElseThrow(()-> new EntityNotFoundException("등록되지 않은 사용자입니다."));
 
         // 2. 이 loginMember의 비밀번호랑 입력받은 dto의 비밀번호 비교
         if(!BCrypt.checkpw(dto.getPassword(), loginMember.getPassword()))
@@ -41,5 +48,7 @@ public class MemberService {
         else {
             return loginMember;
         }
+
+        // 나중에 토큰추가
     }
 }
