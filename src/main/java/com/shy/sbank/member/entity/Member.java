ee82e168744e046c64eddf4a11ebfac87c7f3469
MilanoBeer <tgd5753@gmail.com>
2023-08-24 02:57:18 +0900

    feat: modify Member Entity - add Builder Annotation - add Column Annotation - add Mapping Annotation - add List<Account> account field

--- src/main/java/com/shy/sbank/member/entity/Member.java
+++ src/main/java/com/shy/sbank/member/entity/Member.java
@@ -1,31 +1,38 @@
 package com.shy.sbank.member.entity;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.shy.sbank.account.entity.Account;
+import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Getter @Setter
-@NoArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@ToString(of = {"name", "email", "password"})
 public class Member {
 
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
     @Type(type = "uuid-char")
+    @Column(name = "member_id", nullable = false)
     private UUID id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "email", nullable = false)
     private String email;
+
+    @Column(name = "password", nullable = false)
     private String password;
 
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+    private List<Account> account;
 
     @Builder
     public Member(String name, String email, String password) {
