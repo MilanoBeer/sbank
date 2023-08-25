7cc37493f7213de3e4c2830ffc8d4499bec511e2
MilanoBeer <tgd5753@gmail.com>
2023-08-26 00:32:13 +0900

    feat: Add Enum ProductType

---
+++ src/main/java/com/shy/sbank/product/entity/ProductType.java
@@ -0,0 +1,18 @@
+package com.shy.sbank.product.entity;
+
+public enum ProductType {
+
+    // 예금, 적금, 대출
+    DEPOSIT, SAVINGS, LOAN;
+
+//    private final String type;
+
+//    ProductType(String type) {
+//        this.type = type;
+//    }
+//
+//    public String getType() {
+//        return type;
+//    }
+
+}
