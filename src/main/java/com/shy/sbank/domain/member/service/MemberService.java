17813164df4b493730a893f059ce5f64401149c9
MilanoBeer <tgd5753@gmail.com>
2023-09-04 01:30:47 +0900

    trivial : 주석정리

--- src/main/java/com/shy/sbank/domain/member/service/MemberService.java
+++ src/main/java/com/shy/sbank/domain/member/service/MemberService.java
@@ -1,10 +1,10 @@
-package com.shy.sbank.member.service;
+package com.shy.sbank.domain.member.service;
 
-import com.shy.sbank.member.dto.MemberLoginDto;
-import com.shy.sbank.member.dto.MemberRegisterDto;
-import com.shy.sbank.member.entity.Member;
-import com.shy.sbank.member.exception.PasswordMatchException;
-import com.shy.sbank.member.repository.MemberRepository;
+import com.shy.sbank.domain.member.dto.MemberRegisterDto;
+import com.shy.sbank.domain.member.exception.PasswordMatchException;
+import com.shy.sbank.domain.member.repository.MemberRepository;
+import com.shy.sbank.domain.member.dto.MemberLoginDto;
+import com.shy.sbank.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.mindrot.jbcrypt.BCrypt;
@@ -18,11 +18,9 @@ import javax.persistence.EntityNotFoundException;
 @RequiredArgsConstructor
 @Slf4j
 public class MemberService {
-    // Service는 repository와 인접해있는 계층
     private final MemberRepository memberRepository;
 
     public boolean register(MemberRegisterDto dto) {
-        // TODO : 이메일 중복 처리 ( "이미 가입된 이메일이 존재합니다" )
         if (memberRepository.existsByEmail(dto.getEmail())) throw new IllegalStateException("이미 가입된 정보가 존재합니다.");
 
         Member member = Member.builder()
@@ -32,22 +30,17 @@ public class MemberService {
                 .build();
 
         memberRepository.save(member);
-        // TODO : return true? mebmer의 id?
         return true;
     }
 
     public Member login(MemberLoginDto dto) {
-        // 1. login정보 확인하기 위해, dto의 email -> repo -> db에서 찾아오기
         Member loginMember = memberRepository.findByEmail(dto.getEmail())
                 .orElseThrow(()-> new EntityNotFoundException("등록되지 않은 사용자입니다."));
 
-        // 2. 이 loginMember의 비밀번호랑 입력받은 dto의 비밀번호 비교
         if(!BCrypt.checkpw(dto.getPassword(), loginMember.getPassword()))
             throw new PasswordMatchException("비밀번호가 틀립니다.");
         else {
             return loginMember;
         }
-
-        // 나중에 토큰추가
     }
 }
